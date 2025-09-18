69cb62d61d8989bf8251b607ab253c9ad03c364c
mjh090801abc <moonjunghoon090801@sopeuteuweeoui-MacBookPro.local>
2025-09-18 20:24:55 +0900

    first commit

---
+++ src/main/java/com/example/ToDoList/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.ToDoList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // 개발 편의상 CSRF 끔
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // 모든 요청을 인증 없이 허용
+                )
+                .formLogin(form -> form.disable()); // 스프링 기본 로그인 폼 비활성화.
+
+        return http.build();
+    }
+}
