382ece1dce57b53c72fb1f3cb0b9644edc34db8d
mjh090801abc <mjh090801abc@dgsw.hs.kr>
2025-09-19 11:59:41 +0900

    JpaRepository를 인터페이스 상속을 해 레포지토의 대상되는 엔티티 타입과 해당 엔티티의 PK의 속성 타입을 지정해주는 코드 작성

--- src/main/java/com/example/ToDoList/ToDoRepository.java
+++ src/main/java/com/example/ToDoList/ToDoRepository.java
@@ -2,5 +2,6 @@ package com.example.ToDoList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// JpaRepository의 두번재 제네릭 타입은 entity의 PK 타입과 같아야함
 public interface ToDoRepository extends JpaRepository<ToDoEntity, Integer> {
 }
