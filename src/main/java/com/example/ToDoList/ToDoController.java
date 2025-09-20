caf49316d36c6a3d5a5a218de5f7b46f29a6bcc5
mjh090801abc <mjh090801abc@dgsw.hs.kr>
2025-09-20 16:34:38 +0900

    controller가 service에서 필드(값)을 가져와서 리스트형태로 받아서 html로 보내는 코드

--- src/main/java/com/example/ToDoList/ToDoController.java
+++ src/main/java/com/example/ToDoList/ToDoController.java
@@ -4,22 +4,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
-public class DoToController {
+public class ToDoController {
 
-    private final ToDoRepository toDoRepository;
+    private final ToDoService toDoService;
 
     @RequestMapping("/todo")
     public String list(Model model) {
-        // toDoRepository 안에 있는 Repository를 사용한다는 뜻 (findAll이 있으므로 DB에 저장된 모든 항목을 가져옴)
-        List<ToDoEntity> toDoEntityList = this.toDoRepository.findAll();
+        List<ToDoEntity> toDoEntityList = this.toDoService.getList();
         model.addAttribute("toDoEntityList", toDoEntityList);
         return "todolist";
+
+    }
+
+    @RequestMapping("/")
+    public String root(Model model) {
+        return "redirect:/todo";
     }
 }
